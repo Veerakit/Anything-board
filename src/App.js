2e595167c793d8bc467dd6cb715d36d0349168e8
Veerakit Chotpititaveepongm <air.veerakit@gmail.com>
2022-04-03 02:22:06 +0700

    fix testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Navbar from './Components/Navbar';
+import Input from './Components/Input';
+import Post from './Components/Post';
+
+let id = 1;
+
 function App() {
+  const[posts, setPosts] = useState([]);
+
+  function addPost(title){
+    const newPost = {id, title};
+    setPosts([newPost, ...posts]);
+    id += 1;
+  }
+
+  function deletePost(id){
+    const updatedPosts = posts.filter((posts) => posts.id !== id);
+    setPosts(updatedPosts);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Input addPost={addPost}/>
+      {posts.map((post) => (
+          <Post 
+            key={post.id} 
+            id={post.id} 
+            title={post.title}
+            deletePost={deletePost}
+          />
+        )
+      )}
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <header className="Navbar">Anything BOARD</header>
+  );
+}
+
+export default Navbar;
